a54be7fb585c673753e3f1b51d681a04f5e507e2
jang0414 <0414wkd@naver.com>
2024-10-13 11:34:32 +0900

    Update 1676-1.py

--- 장창우/W1_백준/1676_팩토리얼 0의 개수/1676-1.py
+++ 장창우/W1_백준/1676_팩토리얼 0의 개수/1676-1.py
@@ -1,1 +1,10 @@
+def count_trailing_zeros(n):
+    count = 0
+    while n >= 5:
+        n //= 5
+        count += n
+    return count
 
+# 사용자로부터 N 입력받기
+N = int(input("N을 입력하세요: "))
+print(count_trailing_zeros(N))
