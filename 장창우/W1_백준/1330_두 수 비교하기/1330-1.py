00b41f963f5fea23b07e6eb49a9532e52c914b44
jang0414 <0414wkd@naver.com>
2024-10-13 11:03:57 +0900

    Update 1330-1.py

--- 장창우/W1_백준/1330_두 수 비교하기/1330-1.py
+++ 장창우/W1_백준/1330_두 수 비교하기/1330-1.py
@@ -1,1 +1,11 @@
+# 두 정수 A와 B를 입력받습니다.
+A = int(input("정수 A를 입력하세요: "))
+B = int(input("정수 B를 입력하세요: "))
 
+# A와 B를 비교하여 결과를 출력합니다.
+if A > B:
+    print("A > B")
+elif A < B:
+    print("A < B")
+else:
+    print("A == B")
